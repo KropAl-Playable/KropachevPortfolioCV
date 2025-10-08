012f59daf4c27ae20810a185d1e1343702f38c41
KropAl-Playable <mad.alexis@live.com>
2025-10-08 19:51:13 +0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://kropal-playable.github.io', // ← замени на свой
+  base: '/',                               // по умолчанию и так '/', но пусть явно
   integrations: [tailwind({ applyBaseStyles: true })],
-  site: 'https://example.com',
 });
